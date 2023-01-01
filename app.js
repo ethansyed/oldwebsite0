64d0c7bf05dcdcb3799e6a2cc5da57d6564a3b92
ethansyedUT <ethansyed@gmail.com>
2023-01-01 02:55:05 -0600

    Added skills icons / Scrolling effect(buggy)

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
+const navlink = document.querySelector('.navbar__item')
+
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+});
+
+navLink.addEventListener('click', function(){
+    menu.classList.toggle();
+    menuLinks.classList.toggle();
 });
